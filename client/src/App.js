7929f4538b01d22c4f028e23607f5f9fc35e49fd
Nicholas Watson <ngwatso@Nicholass-iMac.hsd1.mi.comcast.net>
2020-12-05 20:54:40 -0500

    set up routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Route } from "react-router-dom";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
 
-export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
-  const [movieList, setMovieList] = useState([]);
+export default function App() {
+	const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+	const [movieList, setMovieList] = useState([]);
 
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
-          // Study this response with a breakpoint or log statements
-          // and set the response data as the 'movieList' slice of state
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    getMovies();
-  }, []);
+	useEffect(() => {
+		const getMovies = () => {
+			axios.get("http://localhost:5000/api/movies") // Study this endpoint with Postman
+				.then((response) => {
+					// setMovieList()
+					// Study this response with a breakpoint or log statements
+					// and set the response data as the 'movieList' slice of state
+				})
+				.catch((error) => {
+					console.error("Server Error", error);
+				});
+		};
+		getMovies();
+	}, []);
 
-  const addToSavedList = id => {
-    // This is stretch. Prevent the same movie from being "saved" more than once
-  };
+	const addToSavedList = (id) => {
+		// This is stretch. Prevent the same movie from being "saved" more than once
+	};
 
-  return (
-    <div>
-      <SavedList list={[ /* This is stretch */]} />
+	return (
+		<div>
+			<SavedList
+				list={
+					[
+						/* This is stretch */
+					]
+				}
+			/>
 
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+			<Route
+				exact
+				path="/"
+				render={(props) => {
+					<MovieList {...props} />;
+				}}
+			/>
+			{/* <Route exact path="/" component={MovieList} /> */}
+
+			<Route path="/movies/:id" component={Movie} />
+			{/* <div>Replace this Div with your Routes</div> */}
+		</div>
+	);
 }

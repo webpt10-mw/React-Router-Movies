5e8869411e0969a54257d32a1b633f992b283a08
Nicholas Watson <ngwatso@Nicholass-iMac.hsd1.mi.comcast.net>
2020-12-05 21:55:37 -0500

    set up hooks, routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Route } from "react-router-dom";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 import SavedList from "./Movies/SavedList";
 
@@ -12,7 +14,8 @@ export default function App() {
 		const getMovies = () => {
 			axios.get("http://localhost:5000/api/movies") // Study this endpoint with Postman
 				.then((response) => {
-					// setMovieList()
+					setMovieList(response.data);
+					console.log(movieList);
 					// Study this response with a breakpoint or log statements
 					// and set the response data as the 'movieList' slice of state
 				})
@@ -22,7 +25,7 @@ export default function App() {
 		};
 		getMovies();
 	}, []);
-
+	console.log("list", movieList);
 	const addToSavedList = (id) => {
 		// This is stretch. Prevent the same movie from being "saved" more than once
 	};
@@ -40,13 +43,14 @@ export default function App() {
 			<Route
 				exact
 				path="/"
-				render={(props) => {
-					<MovieList {...props} />;
-				}}
+				render={(props) => <MovieList {...props} />}
 			/>
 			{/* <Route exact path="/" component={MovieList} /> */}
 
-			<Route path="/movies/:id" component={Movie} />
+			<Route
+				path="/movies/:id"
+				render={(props) => <Movie {...props} />}
+			/>
 			{/* <div>Replace this Div with your Routes</div> */}
 		</div>
 	);
